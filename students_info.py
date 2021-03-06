2af0f54ce74b4513d01837e1d51446f9461a5861
Ni4ka <ni4ka@Pavels-iMac-Pro.local>
2021-03-07 00:09:13 +0200

    Add file students_info.py

---
+++ students_info.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+students_firstname = "John"
+students_lastname = "Doe"
+students_age = 20
+students_homeland = "USA"
+students_town = "Naples"
+
+print(students_firstname)
+print(students_lastname)
+print(students_age)
+print(students_homeland)
+print(students_town)
